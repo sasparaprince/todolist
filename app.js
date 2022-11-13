56445f2229bba85a158cd88b1c5a5dcbb9ea5804
prince saspara <princesaspara46@gmail.com>
2022-11-13 11:33:57 +0530

    I

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,11 +9,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-prince:Prince3252@cluster0.xkujutz.mongodb.net/todolistDB", { useNewUrlParser: true });
 
+mongoose.connect("mongodb+srv://admin-prince:Prince3252@cluster0.xkujutz.mongodb.net/todolistDB3252", {useNewUrlParser: true, useUnifiedTopology: true});
 const itemsSchema = {
   name: String
 };
@@ -42,12 +43,12 @@ const listSchema = {
 const List = mongoose.model("List", listSchema);
 
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
 
-  Item.find({}, function (err, foundItems) {
+  Item.find({}, function(err, foundItems){
 
     if (foundItems.length === 0) {
-      Item.insertMany(defaultItems, function (err) {
+      Item.insertMany(defaultItems, function(err){
         if (err) {
           console.log(err);
         } else {
@@ -56,18 +57,18 @@ app.get("/", function (req, res) {
       });
       res.redirect("/");
     } else {
-      res.render("list", { listTitle: "Today", newListItems: foundItems });
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
     }
   });
 
 });
 
-app.get("/:customListName", function (req, res) {
+app.get("/:customListName", function(req, res){
   const customListName = _.capitalize(req.params.customListName);
 
-  List.findOne({ name: customListName }, function (err, foundList) {
-    if (!err) {
-      if (!foundList) {
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
         //Create a new list
         const list = new List({
           name: customListName,
@@ -78,7 +79,7 @@ app.get("/:customListName", function (req, res) {
       } else {
         //Show an existing list
 
-        res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
       }
     }
   });
@@ -87,7 +88,7 @@ app.get("/:customListName", function (req, res) {
 
 });
 
-app.post("/", function (req, res) {
+app.post("/", function(req, res){
 
   const itemName = req.body.newItem;
   const listName = req.body.list;
@@ -96,11 +97,11 @@ app.post("/", function (req, res) {
     name: itemName
   });
 
-  if (listName === "Today") {
+  if (listName === "Today"){
     item.save();
     res.redirect("/");
   } else {
-    List.findOne({ name: listName }, function (err, foundList) {
+    List.findOne({name: listName}, function(err, foundList){
       foundList.items.push(item);
       foundList.save();
       res.redirect("/" + listName);
@@ -108,20 +109,20 @@ app.post("/", function (req, res) {
   }
 });
 
-app.post("/delete", function (req, res) {
+app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
 
   if (listName === "Today") {
-    Item.findByIdAndRemove(checkedItemId, function (err) {
+    Item.findByIdAndRemove(checkedItemId, function(err){
       if (!err) {
         console.log("Successfully deleted checked item.");
         res.redirect("/");
       }
     });
   } else {
-    List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function (err, foundList) {
-      if (!err) {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err){
         res.redirect("/" + listName);
       }
     });
@@ -130,10 +131,10 @@ app.post("/delete", function (req, res) {
 
 });
 
-app.get("/about", function (req, res) {
+app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen('port', process.env.PORT || 3000, function () {
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
